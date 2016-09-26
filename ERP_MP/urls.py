f70b953122a85472d6444450b2bf9f0f5acd94db
Nilotpal Roychowdhury <coolnilotpal@gmail.com>
2016-09-27 05:04:53 +0530

    Fixed some internal issues Part 4

--- ERP_MP/urls.py
+++ ERP_MP/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
-#added a comment
+#added a comment again
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('homepage.urls')),
