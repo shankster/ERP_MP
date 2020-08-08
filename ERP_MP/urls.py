0efc93d202ac958ae15b5e26bb7cd3ad377486dc
Nilotpal Roychowdhury <nilotpal.roy05@gmail.com>
2020-08-09 04:02:23 +0530

    Changed URL Patterns from urls to path

--- ERP_MP/urls.py
+++ ERP_MP/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include,url
+from django.urls import include,path
 from django.contrib import admin
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('homepage.urls')),
-    url(r'^medicine/', include('medicine.urls')),
-    url(r'^supplier/', include('supplier.urls')),
-    url(r'^client/', include('client.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('homepage.urls')),
+    path('medicine/', include('medicine.urls')),
+    path('supplier/', include('supplier.urls')),
+    path('client/', include('client.urls')),
 ]
 
 
--- medicine/urls.py
+++ medicine/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = 'medicine'
 
 urlpatterns = [
-    url(r'^', views.index, name='index'),
+    path('', views.index, name='index'),
 ]
